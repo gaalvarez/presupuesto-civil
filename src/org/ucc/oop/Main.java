ac8a837f86f230271a27af8ffafa9fc44a0841fd
gaalvarez <gaalvarez@outlook.com>
2019-08-01 11:24:15 -0500

    fix: mejor práctica para sacar mensajes en consola

--- src/org/ucc/oop/Main.java
+++ src/org/ucc/oop/Main.java
@@ -1,10 +1,14 @@
 package org.ucc.oop;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
+	private static Logger log = Logger.getLogger(Main.class.getName());
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		log.log(Level.WARNING, "HELLO WORLD");
 	}
 
 }
